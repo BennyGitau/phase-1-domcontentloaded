6808825c4b2e4bb1352ac5221ae031c39bdbd51a
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-30 16:46:55 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    let input = document.querySelector('p');
+    input.textContent= 'This is really cool!';
+
+});
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', (e) => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        toDO(e.target.new_todo.value)
+        form.reset()
+
+    });
+})
+
+function toDO (dothis){
+    console.log(dothis)
+    let p = document.createElement('p');
+    let btn = document.createElement('button')
+    btn.addEventListener('click',deleteafter)
+    btn.textContent = 'Del';
+    p.textContent = `${dothis} `
+    p.append(btn)
+    
+
+    document.querySelector('#todo_container').appendChild(p)
+}
+ function deleteafter (e) {
+    e.target.parentNode.remove()
+ }
+ */
+
+ 
